cd88b7f1b8b21e47c771fd84054896947d4b62a1
Amir Pahadi <apahadi73@gmail.com>
2020-08-06 02:32:15 -0500

    Updates

---
+++ precache-manifest.505f52ecfa78bfb0ebee93135255a75f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c782805a45ada94dfc7292001f9551",
+    "url": "/build-recipe-app/index.html"
+  },
+  {
+    "revision": "52cb87d9f027c2414be8",
+    "url": "/build-recipe-app/static/css/main.91b9ae20.chunk.css"
+  },
+  {
+    "revision": "054da12adbc8ed4c31b1",
+    "url": "/build-recipe-app/static/js/2.97f4be64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/build-recipe-app/static/js/2.97f4be64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52cb87d9f027c2414be8",
+    "url": "/build-recipe-app/static/js/main.c0f35763.chunk.js"
+  },
+  {
+    "revision": "d5e60a5b21ddbd69707e",
+    "url": "/build-recipe-app/static/js/runtime-main.4ed426c9.js"
+  }
+]);
